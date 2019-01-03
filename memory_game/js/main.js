013dc5150e2eb017c31c103b4043743afc48f6d3
Konjo Infinity <clients@konjoweb.com>
2019-01-03 12:41:02 -0500

    created memory game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,26 @@
+console.log("Up and running!");
+
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
+
+/*
+Navigate to the main.js file in Sublime.
+
+Create four variables that simulate the cards in your game.
+
+They should be string types.
+
+Two should have a value of "queen", and the other two should have a value of "king".
+
+They will represent your cards (e.g., "queen" card).
+
+Name your variables cardOne, cardTwo, cardThree, and cardFour.
+
+Here's an example of one card:
+
+var cardOne = "queen"; */
